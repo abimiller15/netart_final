94a3a66f7fb31c072438c57effa5a4c5f4d97fc8
Abi Miller <abimiller15@gmail.com>
2018-11-28 15:28:09 -0600

    added blocked domains

---
+++ js/blocked_domains.js
@@ -0,0 +1,8 @@
+var blocked_domains = [
+"*://*.lb.usemaxserver.de/*",
+"*://*.tracking.klickthru.com/*",
+"*://*.pubads.g.doubleclick.net/*",
+"*://*.googleads.g.doubleclick.net/*",
+"*://*.www.remove.com/*"
+"*://*.zmedia.com/*",
+"*://*.zv1.november-lax.com/*"];
