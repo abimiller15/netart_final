609f197bc68487238dd73f2e201f77361a9f0743
homingdevice <mconlo@saic.edu>
2018-12-08 10:53:28 -0600

    ISSUE: chrome says that line 33 "functino currentCat is not defined" ??? don't know why

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,35 @@
 var enabled = true;
+//var cookies = ["https://homingdevice.github.io/"]
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
+	function(details){
+		alert("running")
 		return {cancel: enabled };
 	},
 	{urls: blocked_domains},
 	["blocking"]
 );
+const snippets = [
+  "lb.usemaxserver.de",
+  "tracking.klickthru.com",
+  "pubads.g.doubleclick.net",
+  "googleads.g.doubleclick.net",
+  "*remove.com",
+  "*pagead2.googlesyndication.com",
+  "*www.googletagservices.com",
+  "*zmedia.com",
+  "*s.amazon-adsystem.com",
+  "*aax-us-east.amazon-adsystem.com",
+  "*zv1.november-lax.com"];
+let blockURL = "https://homingdevice.github.io/";
+let cats = document.querySelectorAll('iframe');
+for(let i = 0; i < cats.length; i++){
+	let currentCat = cats[i];
+
+      currentCat.src = blockURL
+    }
+    for(let l = 0; l < snippets.length; l++){
+  	let currentURL = snippets[l]
+  }
+  if( currentCat.src.includes(currentURL) ){
+        currentCat.src = blockURL
+      }
