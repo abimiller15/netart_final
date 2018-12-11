402f1d80bf1ddc67b1af4ee0e6c43dddf89bb01c
Abi Miller <abimiller15@gmail.com>
2018-12-11 10:59:39 -0600

    ah things got weird from a merge (whoops!), i think i fixed them

--- js/sketch.js
+++ js/sketch.js
@@ -13,16 +13,6 @@ const snippets = [
   "tracking.klickthru.com",
   "pubads.g.doubleclick.net",
   "googleads.g.doubleclick.net",
-<<<<<<< HEAD
-  "*remove.com",
-  "*pagead2.googlesyndication.com",
-  "*www.googletagservices.com",
-  "*zmedia.com",
-  "*s.amazon-adsystem.com",
-  "*aax-us-east.amazon-adsystem.com",
-  "*zv1.november-lax.com",
-	"contextual.media.net"];
-=======
   "remove.com",
   "pagead2.googlesyndication.com",
   "www.googletagservices.com",
@@ -32,30 +22,23 @@ const snippets = [
   "zv1.november-lax.com",
   "contextual.media.net",
   "stags.bluekai.com"];
->>>>>>> b9a83adcd8878b931425b8b3ee2650b1ab32300b
 
-let blockURL = "https://homingdevice.github.io/";
-let cats = document.querySelectorAll('ad-iframe');
+  let blockURL = "https://homingdevice.github.io/";
+  let cats = document.querySelectorAll('iframe');
 
-for(let i = 0; i < cats.length; i++){
-	let currentCat = cats[i];
-		currentCat.src = blockURL
-
-
-for(let l = 0; l < snippets.length; l++){
-  	let currentURL = snippets[l]
+  for(let i = 0; i < cats.length; i++){
+  	let currentCat = cats[i];
+  		currentCat.src = blockURL
 
 
-if(currentCat.src.includes(currentURL) ){
-    	currentCat.src = blockURL
+  for(let l = 0; l < snippets.length; l++){
+    	let currentURL = snippets[l]
 
+
+  if(currentCat.src.includes(currentURL) ){
+      	currentCat.src = blockURL
+        }
       }
-    }
-<<<<<<< HEAD
-	}
-	alert(blockURL) 
-=======
-}
-	// alert(blockURL)
-	// alert("working!")
->>>>>>> b9a83adcd8878b931425b8b3ee2650b1ab32300b
+  }
+  	// alert(blockURL)
+  // alert("working!")
