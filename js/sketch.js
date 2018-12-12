901d8a5ae0ebd186531776ef473b5c899a22856a
homingdevice <mconlo@saic.edu>
2018-12-11 21:56:01 -0600

    moved extra files basic extension

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,21 @@
 var enabled = true;
-//var cookies = ["https://homingdevice.github.io/"]
-chrome.webRequest.onBeforeRequest.addListener(
+//var cookies = ["https://homingdevice.github.io/"]var blocked_domains = [
+//var blocked_domains = [
+// "*://*.lb.usemaxserver.de/*",
+// "*://*.tracking.klickthru.com/*",
+// "*://*.pubads.g.doubleclick.net/*",
+// "*://*.googleads.g.doubleclick.net/*",
+// "*://*.remove.com/*",
+// "*://*.pagead2.googlesyndication.com/*",
+// "*://*.www.googletagservices.com/*",
+// "*://*.zmedia.com/*",
+// "*://*.s.amazon-adsystem.com/*",
+// "*://*.aax-us-east.amazon-adsystem.com/*",
+// "*://*.zv1.november-lax.com/*",
+// "*://*.contextual.media.net/*",
+// "*://*.stags.bluekai.com/*"];
+
+//chrome.webRequest.onBeforeRequest.addListener(
 	function(details){
 		return {cancel: enabled };
 	},
